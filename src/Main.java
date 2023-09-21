702d4412d88cc51e4655aa1ce9f93603381dc9c7
KyleChan <kylec2026@student.cis.edu.hk>
2023-09-21 09:08:03 +0800

    nn

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+/* I have neither given nor received any unauthorised aid on this piece of work */
 public class Main {
 
     public static void main(String[] args) {
-
+        int resultOne = add(1, 2);
+        System.out.println(resultOne);
+        String resultTwo = greeting("Kyle");
+        System.out.println(resultTwo);
+        int resultThree = add(1, 2, 3, 4);
+        System.out.println(resultThree);
+        String print = printMe("Hope you have a wonderful day!");
+        System.out.println(print);
     }
 
+    public static int add(int numOne, int numTwo){
+        return numOne + numTwo;
+    }
+    public static String greeting(String name){
+        return "Bonjour, " + name;
+    }
+    public static int add(int numOne, int numTwo, int numThree, int numFour){
+        int first = add(numOne, numTwo);
+        int second = add(numThree, numFour);
+        return first + second;
+    }
+    public static String printMe(String toBePrinted){
+        return toBePrinted;
+    }
 }
